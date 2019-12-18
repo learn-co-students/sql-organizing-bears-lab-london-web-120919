6b968864ca2bb6d742786e1139c65335061a5471
rmiri <re_miriuk@hotmail.com>
2019-12-18 10:46:58 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+-- INSERT INTO table (column1,column2 ,..)
+-- VALUES( value1,    value2 ,...);
+INSERT INTO bears (name)
+VALUES
+("Mr. Chocolate"),
+("Rowdy"),
+("Tabitha"),
+("Sergeant Brown"),
+("Melissa"),
+("Grinch"),
+("Wendy"),
+(NULL);
+
